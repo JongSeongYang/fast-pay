9e55189c75abfb74d7b294cd23ced34f5721bd12
JongSeong Yang <yangjs0523@naver.com>
2023-08-27 23:59:14 +0900

    feat: RegisterMembershipController

--- membership-service/src/main/java/com/example/membership/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/example/membership/adapter/in/web/RegisterMembershipController.java
@@ -4,15 +4,36 @@ import common.WebAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @WebAdapter
 @RestController
 @RequiredArgsConstructor
-public class RegisterMembershipController {
+class RegisterMembershipController {
 
-    @PostMapping(path = "/register")
-    void registerMembership(){
-        System.out.println("Hello world");
+    private final RegisterMembershipUseCase registerMembershipUseCase;
+    @PostMapping(path = "/membership/register/")
+    void registerMembership(@RequestBody RegisterMembershipRequest request){
+        // RegisterMembershipRequest
+        // name, address, email
+        RegisterMembershipCommand command = RegisterMembershipCommand.builder()
+                .name(request.getName())
+                .address(request.getAddress())
+                .email(request.getEmail())
+                .build();
+        registerMembershipUseCase.registerMembership(command);
+    }
+
+
+    @PostMapping(path = "/membership/axon-register/")
+    void registerAxonMembership(@RequestBody RegisterMembershipRequest request){
+        RegisterMembershipCommand command = RegisterMembershipCommand.builder()
+                .name(request.getName())
+                .address(request.getAddress())
+                .email(request.getEmail())
+                .build();
+
+        registerMembershipUseCase.registerAxonMembership(command);
     }
 }
